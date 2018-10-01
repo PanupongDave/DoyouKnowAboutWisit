262526cb6286c1c1147a2f4821518dc50a11fac1
pozterz <tharathep@pirsquare.net>
2018-10-01 12:35:10 +0700

    add his profile picture

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={'https://graph.facebook.com/100002420637495/picture?width=800'} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Wisit, He come from panga</h1>
         </header>
         <p className="App-intro">
